2c63139215ebd1387547c3ca555f36145899546f
cheungbh <534660436@qq.com>
2025-01-10 09:55:29 +0800

    fix sth

--- analyser/criterion/low_speed.py
+++ analyser/criterion/low_speed.py
@@ -7,22 +7,23 @@ class LowSpeedChecker:
         self.frame_duration = 10
         self.flag = False
 
-    def process(self, candidates):
-        candidates = self.preprocess(candidates)
-        self.status = []
-        for tid, candidate in candidates.items():
-            if len(candidate) < self.frame_duration:
-                self.status.append(False)
-            else:
-                speed = check_speed_ls(candidate)
-                if speed < self.speed_threshold:
-                    self.status.append(False)
-                else:
-                    self.status.append(True)
-        return self.status
+    def process(self, players1, player2, balls, **kwargs):
+        pass
+        # candidates = self.preprocess(candidates)
+        # self.status = []
+        # for tid, candidate in candidates.items():
+        #     if len(candidate) < self.frame_duration:
+        #         self.status.append(False)
+        #     else:
+        #         speed = check_speed_ls(candidate)
+        #         if speed < self.speed_threshold:
+        #             self.status.append(False)
+        #         else:
+        #             self.status.append(True)
+        # return self.status
 
     def visualize(self, frame):
-        if not self.status[-1]:
+        if not self.flag:
             cv2.putText(frame, "Normal speed", (100, 1000), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),
                         2, cv2.LINE_AA)
         else:
