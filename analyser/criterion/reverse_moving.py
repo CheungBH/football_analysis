d0b137a49a550c1276b5b1a58ac102d34f318efc
BookerXU <bookerxu@163.com>
2025-01-14 14:30:11 +0800

    add reverse

--- analyser/criterion/reverse_moving.py
+++ analyser/criterion/reverse_moving.py
@@ -1,20 +1,51 @@
 import cv2
+from collections import defaultdict
+from .utils import calculate_vector, calculate_angle_between_vectors
 
 
 class MovingReverseChecker:
-    def __init__(self):
-        self.th
+    def __init__(self, **kwargs):
+        self.angle_threshold = 135
+        self.frame_duration = 10
+        self.flag = False
+        self.colors = [(0, 0, 255), (125, 125, 125)]
+        self.curve_duration = 10
+        self.base_vector = calculate_vector((0, 0), (0, 1))
 
-    def process(self, balls, players1, players2, **kwargs):
-        pass
+    def process(self, players1, players2, balls, **kwargs):
+        self.team1_dict = players1
+        self.team2_dict = players2
+        self.balls = balls
+        self.angles = [defaultdict(float), defaultdict(float)]
+        for team_id, team in enumerate([players1, players2]):
+            for p_id, position in team.items():
+                if len(position) >= self.frame_duration:
+                    vector = calculate_vector(position[0], position[-1])
+                    angle = calculate_angle_between_vectors(self.base_vector, vector)
+                    self.angles[team_id][p_id] = angle
+                    if angle > self.angle_threshold:
+                        self.flag = True
 
     def visualize(self, frame):
-        if not self.status[-1]:
-            cv2.putText(frame, "Normal speed", (100, 1000), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),
+        if not self.flag:
+            cv2.putText(frame, "Normal", (100, 1000), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0),
                         2, cv2.LINE_AA)
         else:
-            cv2.putText(frame, "Low Speed", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),
+            cv2.putText(frame, "Someone is reverse", (100, 1000), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255),
                         2, cv2.LINE_AA)
 
     def visualize_details(self, frame):
         self.visualize(frame)
+
+        for t_idx, (color, team_dict) in enumerate(zip(self.colors, [self.team1_dict, self.team2_dict])):
+            for p_idx, (player, locations) in enumerate(team_dict.items()):
+                angle = self.angles[t_idx][player]
+                cv2.putText(frame, "id {}: Angle {}".format(player, angle), (100 + t_idx * 500, 100 + p_idx * 50),
+                            cv2.FONT_HERSHEY_PLAIN, 2, color, thickness=2)
+
+    def vis_path(self, frame, locations, vis_duration, color):
+        for i in range(vis_duration):
+            cv2.circle(frame, (int(locations[-i][0]), int(locations[-i][1])), 20, color, -1)
+        for j in range(vis_duration-1):
+            cv2.line(frame, (int(locations[-j][0]), int(locations[-j][1])),
+                     (int(locations[-(j-1)][0]), int(locations[-j-1][1])), color, 3)
--- analyser/criterion/utils.py
+++ analyser/criterion/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def check_speed_ls(location_ls):
@@ -7,3 +8,30 @@ def check_speed_ls(location_ls):
         speed += abs(np.linalg.norm(np.array(location_ls[i]) - np.array(location_ls[i+1])))
     return speed/(len(location_ls)-1)
 
+
+def calculate_vector(point1, point2):
+    vector = (point2[0] - point1[0], point2[1] - point1[1])
+    return vector
+
+
+def calculate_angle_between_vectors(vector1, vector2):
+    dot_product = vector1[0] * vector2[0] + vector1[1] * vector2[1]
+    magnitude_vector1 = math.sqrt(vector1[0]**2 + vector1[1]**2)
+    magnitude_vector2 = math.sqrt(vector2[0]**2 + vector2[1]**2)
+    angle_radians = math.acos(dot_product / (magnitude_vector1 * magnitude_vector2))
+    angle_degrees = math.degrees(angle_radians)
+    return angle_degrees
+
+
+point1 = (0, 0)
+point2 = (0, 1)
+point3 = (1, 1)
+
+vector1 = calculate_vector(point1, point2)
+vector2 = calculate_vector(point2, point3)
+
+angle_between_vectors = calculate_angle_between_vectors(vector1, vector2)
+
+print("Vector between point1 and point2:", vector1)
+print("Vector between point2 and point3:", vector2)
+print("Angle between the two vectors:", angle_between_vectors, "degrees")
