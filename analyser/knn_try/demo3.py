a891767b9442a292ec4ccd5db08ff2948bdc5520
cheungbh <cheungbh@cheungbhdeMacBook-Pro.local>
2025-02-16 01:46:56 +0800

    add second voting

--- analyser/knn_try/demo3.py
+++ analyser/knn_try/demo3.py
@@ -18,6 +18,7 @@ def filter_colors(clustered_colors, team_colors, threshold=80):
     filtered_colors_raw = []
     filtered_colors = []
     indices = []
+    similarities = []
 
     for index, color1 in enumerate(clustered_colors):
         min_similarity = float('inf')
@@ -31,16 +32,24 @@ def filter_colors(clustered_colors, team_colors, threshold=80):
 
         if min_similarity < threshold:
             filtered_colors_raw.append([int(c) for c in color1.tolist()])
-            filtered_colors.append((closest_color, min_similarity))
+            filtered_colors.append(closest_color)
+            similarities.append(min_similarity)
             indices.append(index)
 
-    return filtered_colors_raw, filtered_colors, indices
+    return filtered_colors_raw, filtered_colors, indices, similarities
 
 
 def process_images(folder_path, team_colors):
     dominant_colors = []
     for file_name in os.listdir(folder_path):
         if file_name.endswith('.jpg') or file_name.endswith('.png'):
+            full_img = np.zeros((900, 1500, 3), np.uint8)
+            # hsv_team_colors = [cv2.cvtColor(np.uint8([[color]]), cv2.COLOR_BGR2HSV)[0][0].tolist() for color in team_colors]
+            for i, color in enumerate(team_colors):
+                full_img[0:300, i * 300:(i + 1) * 300] = color  # 第一行
+                cv2.putText(full_img, f'G:{team_colors[i][0]} B:{team_colors[i][1]} R:{team_colors[i][2]}',
+                            (i * 300 + 10, 150),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
             image = cv2.imread(os.path.join(folder_path, file_name))
             image_2d = image.reshape(-1, 3)
 
@@ -66,38 +75,66 @@ def process_images(folder_path, team_colors):
                 cv2.putText(full_img, f'ratios:{round(ratios[i],2)}', (i * 300 + 50, 550),  # 添加索引值
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
 
-            filtered_colors_raw, filtered_colors, indices = filter_colors(cluster_centers, team_colors)
+            filtered_colors_raw, filtered_colors, indices, similarity = filter_colors(cluster_centers, team_colors)
 
             # 在第三行绘制过滤后的颜色块和相似度值
-            for i in range(len(filtered_colors)):
-                full_img[600:900, indices[i] * 300:(indices[i] + 1) * 300] = filtered_colors[i][0]  # 第三行
-                cv2.putText(full_img, f'similarity:{str(int(filtered_colors[i][1]))}', (indices[i] * 300 + 50, 750),
+            for i in range(len(filtered_colors_raw)):
+                full_img[600:900, indices[i] * 300:(indices[i] + 1) * 300] = filtered_colors[i]  # 第三行
+                cv2.putText(full_img, 'similarity:{}'.format(similarity[i]), (indices[i] * 300 + 50, 750),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
-                cv2.putText(full_img, f'Index: {indices[i]}', (indices[i] * 300 + 50, 800),  # 添加索引值
+                # cv2.putText(full_img, f'Index: {indices[i]}', (indices[i] * 300 + 50, 800),  # 添加索引值
+                #             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
+
+            # key = cv2.waitKey(0)
+            # Keep the unique colors from a list of colors
+            filtered_team_color = list(set([tuple(color) for color in filtered_colors]))
+
+            second_color = np.array(filtered_team_color)
+            # initial_centers = np.array(filtered_colors)
+            kmeans_2 = KMeans(n_clusters=len(second_color), init=second_color, n_init=1) # init  team_colors
+            kmeans_2.fit(second_color)
+            labels_2 = kmeans_2.predict(image_2d)
+            # labels_2 = kmeans_2.labels_
+            label_counts_2 = np.bincount(labels_2)
+            # label_counts = np.bincount(labels)
+            # 计算每个类的占有比例
+            total_samples = len(labels_2)
+            ratios_2 = label_counts_2 / total_samples
+            team_clustered_colors2 = kmeans_2.cluster_centers_
+            new_img = np.zeros((600, 300*len(team_clustered_colors2), 3), np.uint8)
+
+            filtered_colors_raw_2, filtered_colors_2, indices_2, similarity_2 = filter_colors(team_clustered_colors2, filtered_colors, threshold=1000)
+
+            for i, color in enumerate(filtered_team_color):
+                new_img[0:300, i * 300:(i + 1) * 300] = color  # 第一行
+                cv2.putText(new_img, f'G:{filtered_team_color[i][0]} B:{filtered_team_color[i][1]} R:{filtered_team_color[i][2]}',
+                            (i * 300 + 10, 150),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
 
+            for i, color in enumerate(filtered_colors_raw_2):
+                new_img[300:600, i * 300:(i + 1) * 300] = color  # 第二行
+                cv2.putText(new_img, f'G:{int(filtered_colors_raw_2[i][0])} B:{int(filtered_colors_raw_2[i][1])} R:{int(filtered_colors_raw_2[i][2])}',
+                            (i * 300 + 10, 450),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(new_img, f'Index: {i} ', (i * 300 + 50, 500),  # 添加索引值
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
+                cv2.putText(new_img, f'ratios:{round(ratios_2[i],2)}', (i * 300 + 50, 550),  # 添加索引值
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.imshow('cluster_second',new_img)
+            cv2.imshow('full_img', full_img)
+            cv2.imshow('raw', image)
+            cv2.waitKey(0)
 
-            cv2.imshow('full_img',full_img)
-            cv2.imshow('raw',image)
-            key = cv2.waitKey(0)
-            second_color = np.array(filtered_colors_raw)
-            initial_centers = np.array(filtered_colors)
-            kmeans_2 = KMeans(n_clusters=len(initial_centers), init=initial_centers, n_init=1) # init  team_colors
-            #kmeans_2.fit(second_color)
 
+            # Get color with the most samples
+            # dominant_color = kmeans_2.cluster_centers_[np.argmax(label_counts_2)].astype(int)
 
-
-    return dominant_colors
+    # return dominant_colors
 
 # 获取A文件夹中的所有图片的第一个像素的颜色
-team_colors = get_first_pixel_colors('/media/hkuit164/Backup/football_analysis/output/team_colors')
-full_img = np.zeros((900, 1500, 3), np.uint8)
-#hsv_team_colors = [cv2.cvtColor(np.uint8([[color]]), cv2.COLOR_BGR2HSV)[0][0].tolist() for color in team_colors]
-for i, color in enumerate(team_colors):
-        full_img[0:300, i * 300:(i + 1) * 300] = color  # 第一行
-        cv2.putText(full_img, f'G:{team_colors[i][0]} B:{team_colors[i][1]} R:{team_colors[i][2]}', (i * 300 + 10, 150),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 1, cv2.LINE_AA)
-dominant_colors = process_images('/media/hkuit164/Backup/football_analysis/output/111', team_colors)
+team_colors = get_first_pixel_colors('knn_assets/team_colors')
+
+dominant_colors = process_images('knn_assets/111', team_colors)
 # 打印结果
 for i, color in enumerate(dominant_colors):
     print(f"图片 {i+1} 的主导颜色: {color}")
