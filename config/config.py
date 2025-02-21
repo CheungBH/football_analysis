0938ef8dd731edbf1fb787ab9ebc39f6e494b72a
cheungbh <534660436@qq.com>
2025-02-21 14:14:09 +0800

    dasnjkdhkasj

--- config/config.py
+++ config/config.py
@@ -1,16 +1,15 @@
 topview_height = 400
 topview_width = 800
 
-real_video_height = 1280
+real_video_height = 1080
 real_video_width = 1920
 
 check_action = ["ball_out_range","side_referee"]
 
 cam4_videos_name = \
     [
-        "1.mp4",
-        "2.mp4",
-        "3.mp4",
-        "4.mp4",
-
+        "output_video1.mp4",
+        "output_video2.mp4",
+        "output_video3.mp4",
+        "output_video4.mp4",
     ]
