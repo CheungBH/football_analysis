f5b6af39b90ceeefb4fdb79ed30cd9f72b5e64de
cheungbh <534660436@qq.com>
2025-02-23 12:53:17 +0800

    fix sth

--- auto_run.py
+++ auto_run.py
@@ -2,8 +2,8 @@ import os
 
 video_folder = r"D:\tmp\2.18"
 # videos_path = [os.path.join(video_folder, video) for video in os.listdir(video_folder)]
-output_folder = r"D:\tmp\2.18_output"
-os.makedirs(output_folder, exist_ok=True)
+output_folder_root = r"D:\tmp\2.18_output2"
+os.makedirs(output_folder_root, exist_ok=True)
 model_path = "assets/checkpoints/best.pt"
 court_img = r"D:\tmp\2.18\video_set_1474\court.jpg"
 
@@ -11,7 +11,7 @@ cmd_tpl = "python yolov9_bytetrack_pth_4_cropping.py --save_cropped_humans {} --
 
 for video in os.listdir(video_folder):
     video_path = os.path.join(video_folder, video)
-    output_folder = os.path.join(output_folder, video.split('.')[0])
+    output_folder = os.path.join(output_folder_root, video.split('.')[0])
     output_dir = os.path.join(output_folder, "output")
     output_video = os.path.join(output_folder, "output_video.mp4")
     # output_topview = os.path.join(output_folder, "topview.jpg")
