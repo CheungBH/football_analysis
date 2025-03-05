53beac46fb4cb906a8c2047701e464c86f690457
BookerXU <bookerxu@163.com>
2025-03-05 14:34:07 +0800

    referee in zone

--- analyser/criterion/poor_position.py
+++ analyser/criterion/poor_position.py
@@ -1,6 +1,6 @@
 import cv2
 from .utils import is_in_rectangle, calculate_ratio,is_point_in_triangle
-from collections import defaultdict
+from collections import defaultdict,Counter
 
 class PoorPositionChecker:
     def __init__(self, **kwargs):
@@ -10,7 +10,7 @@ class PoorPositionChecker:
         self.frame_duration = 10
         self.flag_list =[]
 
-    def process(self, players,balls, frame_queue, **kwargs):
+    def process(self, players,balls, color,frame_queue, **kwargs):
         self.team_dict = players
         self.flag = False
         court = [(50, 50), (1100, 730)]
@@ -25,6 +25,7 @@ class PoorPositionChecker:
             rect2_ball = [(960,50), (1200, 720)]
             rect1_values, rect2_values = [],[]
             rect1_id,rect2_id =[],[]
+            rect1_color,rect2_color=[],[]
 
             for p_id, positions in players.items():
                 if len(positions) >= self.frame_duration and positions[-1] != [-1,-1] and positions[-self.frame_duration] != [-1,-1]:
@@ -38,16 +39,24 @@ class PoorPositionChecker:
                     if is_in_rectangle(v_position, rect1):
                         rect1_values.append(v_position)
                         rect1_id.append(v_id)
+                        filter_color_list = [x for x in color[v_id] if x != -1]
+                        counter = Counter(filter_color_list)
+                        team_color = counter.most_common(1)[0][0] if counter else 0
+                        rect1_color.append(team_color)
                     elif is_in_rectangle(v_position, rect2):
                         rect2_values.append(v_position)
                         rect2_id.append(v_id)
-
-            if 1<len(rect1_values) < 4 and is_in_rectangle(ball,rect1_ball): # ball_position
+                        filter_color_list = [x for x in color[v_id] if x != -1]
+                        counter = Counter(filter_color_list)
+                        team_color = counter.most_common(1)[0][0] if counter else 0
+                        rect2_color.append(team_color)
+            referee = 4
+            if 1<len(rect1_values) < 4 and is_in_rectangle(ball,rect1_ball) and referee not in rect1_color: # ball_position
                 goal_keeper = min(rect1_values, key=lambda point: point[0])
                 upper_door = [50,349]
                 lower_door = [50,421]
                 self.flag_list.append(not is_point_in_triangle(goal_keeper,ball,upper_door,lower_door))
-            elif 1<len(rect2_values) < 4 and is_in_rectangle(ball,rect2_ball):
+            elif 1<len(rect2_values) < 4 and is_in_rectangle(ball,rect2_ball) and referee not in rect2_color:
                 goal_keeper = max(rect2_values, key=lambda point: point[0])
                 upper_door = [1100,349]
                 lower_door = [1100,421]
